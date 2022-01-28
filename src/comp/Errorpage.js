43d74b8de55052f920b0cb2732f8d7872a9c8fd6
Nitin Sain <nitin332709@gmail.com>
2022-01-28 15:21:21 +0530

    404 Error Page is added

--- src/comp/Errorpage.js
+++ src/comp/Errorpage.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Error from '../Assates/Images/Error.jpg';
+
+const ErrorImg = {
+  width: "100%",
+  height: '100vh',
+  backgroundImage: `url(${Error})`,
+  backgroundRepeat: 'none',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover'
+}
 
 const Errorpage = () => {
   return (
-      <h1>Error page</h1>
+    <>
+      <div style={ErrorImg}>
+      </div>
+    </>
   )
 };
 
